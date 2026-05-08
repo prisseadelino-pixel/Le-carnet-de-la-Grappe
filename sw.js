b6106769fde0a7918fd88835cc1540dda2df3751
prisseadelino-pixel <prisseadelino@gmail.com>
2026-05-08 17:13:01 +0200

    Implement service worker caching for offline support

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
-self.addEventListener('fetch', function(event) {
-  // Indispensable pour le mode standalone
+const CACHE_NAME = 'grappe-v1';
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['index.html'])));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
