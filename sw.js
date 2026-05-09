67cff7418873db4384568f52adf95f6139e2c770
prisseadelino-pixel <prisseadelino@gmail.com>
2026-05-09 09:56:48 +0200

    Refactor service worker event listeners for clarity

--- sw.js
+++ sw.js
@@ -1,7 +1,13 @@
 const CACHE_NAME = 'grappe-v1';
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['index.html'])));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(['index.html']))
+  );
 });
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
